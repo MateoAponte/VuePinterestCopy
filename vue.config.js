4cad43b1d4e0167b5ae249b4d7fd86e247122351
Mateo Ap <mateo.aponte@masivian.com>
2023-05-18 13:42:30 -0500

    fix: Solve conflict in libraries

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-
-module.exports = {
-	publicPath: process.env.NODE_ENV === "production" ? "/VuePinterestCopy/build/" : "/",
-	outputDir: path.resolve(__dirname, "./build"),
-	assetsDir: "./",
-};
